0d72bae6448beb57a3f03cf2c771e77c14ac6ddc
yu <625571185@qq.com>
2020-12-04 13:47:26 +0800

    init

---
+++ testTable/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testTable
+//
+//  Created by yu on 2019/7/22.
+//  Copyright © 2019 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testTable/ViewController.m
@@ -0,0 +1,47 @@
+//
+//  ViewController.m
+//  testTable
+//
+//  Created by yu on 2019/7/22.
+//  Copyright © 2019 yu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+#pragma mark - UITableViewDelegate && UITableViewDataSource
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 13;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableCell"];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+}
+
+-(void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    
+}
+
+
+@end
